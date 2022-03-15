223c5dee26f4e168eaa234e6d879038da7091021
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-03-15 12:47:48 -0300

    fix: move dashboard page styles to another folder

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,8 +1,8 @@
 import { Aside } from '@components/application/dashboard/aside'
 import { ContentArea } from '@components/application/dashboard/content-area'
 import { Topnav } from '@components/application/dashboard/topnav'
+import { Container } from '@styles/pages/dashboard/styles'
 import type { NextPage } from 'next'
-import { Container } from './styles'
 
 const Dashboard: NextPage = () => {
     return (
