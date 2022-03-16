843146a4f015259e4651169ce77ce035814f3a97
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-03-16 20:06:07 -0300

    feat: add secondary card chart

--- src/common/components/application/dashboard-page/cards/secondary-card/secondary-card.tsx
+++ src/common/components/application/dashboard-page/cards/secondary-card/secondary-card.tsx
@@ -1,9 +1,110 @@
-import { Container } from './styles'
+import {
+    CartesianGrid,
+    Line,
+    LineChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+} from 'recharts'
+import {
+    Chart,
+    Container,
+    Dot,
+    Header,
+    Plan,
+    PlanHeader,
+    PlanName,
+    PlanNumber,
+    Plans,
+} from './styles'
+
+const data = [
+    { name: 'Jan', Internet: 153, Móvel: 323, TV: 123 },
+    { name: 'Fev', Internet: 535, Móvel: 322, TV: 32 },
+    { name: 'Mar', Internet: 123, Móvel: 423, TV: 242 },
+    { name: 'Abr', Internet: 312, Móvel: 132, TV: 332 },
+    { name: 'Mai', Internet: 646, Móvel: 423, TV: 535 },
+    { name: 'Jun', Internet: 321, Móvel: 523, TV: 231 },
+    { name: 'Jul', Internet: 231, Móvel: 434, TV: 123 },
+    { name: 'Ago', Internet: 321, Móvel: 423, TV: 111 },
+    { name: 'Set', Internet: 111, Móvel: 523, TV: 98 },
+    { name: 'Out', Internet: 32, Móvel: 623, TV: 32 },
+    { name: 'Nov', Internet: 122, Móvel: 753, TV: 123 },
+    { name: 'Dez', Internet: 88, Móvel: 323, TV: 323 },
+]
 
 export const SecondaryCard = () => {
     return (
         <Container>
-            <h1>Aside Card 1</h1>
+            <Header>
+                <span>Estatísticas de vendas</span>
+
+                <Plans>
+                    <Plan>
+                        <PlanHeader>
+                            <Dot color="green" />
+                            <PlanName>Internet</PlanName>
+                        </PlanHeader>
+
+                        <PlanNumber>4123</PlanNumber>
+                    </Plan>
+
+                    <Plan>
+                        <PlanHeader>
+                            <Dot color="purple" />
+                            <PlanName>Móvel</PlanName>
+                        </PlanHeader>
+
+                        <PlanNumber>2323</PlanNumber>
+                    </Plan>
+
+                    <Plan>
+                        <PlanHeader>
+                            <Dot color="yellow" />
+                            <PlanName>TV</PlanName>
+                        </PlanHeader>
+
+                        <PlanNumber>1232</PlanNumber>
+                    </Plan>
+                </Plans>
+            </Header>
+
+            <Chart>
+                <ResponsiveContainer width="100%" height={150}>
+                    <LineChart
+                        width={550}
+                        height={250}
+                        data={data}
+                        margin={{ top: 5, right: 30, left: 20 }}
+                    >
+                        <Line
+                            type="monotone"
+                            dataKey="Internet"
+                            stroke="#4BDE97"
+                            dot={false}
+                        />
+                        <Line
+                            type="monotone"
+                            dataKey="Móvel"
+                            stroke="#7433FF"
+                            dot={false}
+                        />
+                        <Line
+                            type="monotone"
+                            dataKey="TV"
+                            stroke="#FFB648"
+                            dot={false}
+                        />
+                        <CartesianGrid
+                            stroke="#33334B"
+                            horizontal={false}
+                            vertical={false}
+                        />
+                        <XAxis dataKey="name" />
+                        <Tooltip />
+                    </LineChart>
+                </ResponsiveContainer>
+            </Chart>
         </Container>
     )
 }
--- src/common/components/application/dashboard-page/cards/secondary-card/styles.ts
+++ src/common/components/application/dashboard-page/cards/secondary-card/styles.ts
@@ -1,6 +1,74 @@
 import styled from 'styled-components'
 
+interface IDotProps {
+    color: 'green' | 'purple' | 'blue' | 'yellow'
+}
+
 export const Container = styled.div`
     grid-area: secondary-card;
     background-color: ${({ theme }) => theme.colors.background_800};
+
+    width: 100%;
+    height: 100%;
+
+    padding: 1.25rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+export const Chart = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+export const Header = styled.header`
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    color: ${({ theme }) => theme.fontColor.title};
+
+    margin-bottom: 1.25rem;
+`
+
+export const Plans = styled.div`
+    display: flex;
+`
+
+export const Plan = styled.div`
+    margin-left: 2rem;
+
+    display: flex;
+    flex-direction: column;
+`
+
+export const PlanHeader = styled.header`
+    display: flex;
+    align-items: center;
+`
+
+export const Dot = styled.span<IDotProps>`
+    display: block;
+    height: 0.5rem;
+    width: 0.5rem;
+    border-radius: 50%;
+    background-color: ${({ theme, color }) =>
+        color === 'green'
+            ? theme.colors.success
+            : color === 'purple'
+            ? theme.colors.secondary
+            : color === 'blue'
+            ? theme.colors.info
+            : theme.colors.warning};
+
+    margin-right: 1rem;
+`
+
+export const PlanName = styled.span``
+
+export const PlanNumber = styled.span`
+    align-self: flex-end;
 `
