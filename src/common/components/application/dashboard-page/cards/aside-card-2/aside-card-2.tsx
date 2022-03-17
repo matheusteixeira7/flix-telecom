b1a52aaa154cc777d2e301db051f145bef9df8f6
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-03-17 12:03:12 -0300

    feat: add aside card 2

--- src/common/components/application/dashboard-page/cards/aside-card-2/aside-card-2.tsx
+++ src/common/components/application/dashboard-page/cards/aside-card-2/aside-card-2.tsx
@@ -1,9 +1,53 @@
-import { Container } from './styles'
+import {
+    Container,
+    Seller,
+    SellerCity,
+    SellerName,
+    SellerNumber,
+    SellerWrapper,
+    Title,
+} from './styles'
 
 export const AsideCard2 = () => {
     return (
         <Container>
-            <h1>Aside Card 1</h1>
+            <Title>Ranking de vendedores</Title>
+
+            <Seller>
+                <SellerWrapper>
+                    <SellerName>Marcos Rocha</SellerName>
+                    <SellerCity>Resende</SellerCity>
+                </SellerWrapper>
+
+                <SellerNumber>+462</SellerNumber>
+            </Seller>
+
+            <Seller>
+                <SellerWrapper>
+                    <SellerName>Diego Ribeiro</SellerName>
+                    <SellerCity>Resende</SellerCity>
+                </SellerWrapper>
+
+                <SellerNumber>+462</SellerNumber>
+            </Seller>
+
+            <Seller>
+                <SellerWrapper>
+                    <SellerName>Teresa Cristina</SellerName>
+                    <SellerCity>Resende</SellerCity>
+                </SellerWrapper>
+
+                <SellerNumber>+462</SellerNumber>
+            </Seller>
+
+            <Seller>
+                <SellerWrapper>
+                    <SellerName>Marisa Miranda</SellerName>
+                    <SellerCity>Resende</SellerCity>
+                </SellerWrapper>
+
+                <SellerNumber>+462</SellerNumber>
+            </Seller>
         </Container>
     )
 }
--- src/common/components/application/dashboard-page/cards/aside-card-2/styles.ts
+++ src/common/components/application/dashboard-page/cards/aside-card-2/styles.ts
@@ -3,4 +3,47 @@ import styled from 'styled-components'
 export const Container = styled.div`
     grid-area: aside-card-2;
     background-color: ${({ theme }) => theme.colors.background_800};
+
+    padding: 1.25rem;
+
+    border-radius: 10px;
+`
+
+export const Title = styled.span`
+    font-size: ${({ theme }) => theme.fontSize.base};
+    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+    color: ${({ theme }) => theme.fontColor.label};
+`
+
+export const Seller = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    padding-bottom: 1rem;
+    margin-top: 1rem;
+
+    border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+`
+
+export const SellerWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const SellerName = styled.span`
+    font-size: ${({ theme }) => theme.fontSize.text_xs};
+    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+    color: ${({ theme }) => theme.fontColor.label};
+`
+
+export const SellerCity = styled.span`
+    font-size: ${({ theme }) => theme.fontSize.text_xs};
+    font-weight: ${({ theme }) => theme.fontWeight.regular};
+    color: ${({ theme }) => theme.fontColor.label2};
+`
+
+export const SellerNumber = styled.span`
+    font-size: ${({ theme }) => theme.fontSize.text_xs};
+    font-weight: ${({ theme }) => theme.fontWeight.semibold};
+    color: ${({ theme }) => theme.colors.success};
 `
