a02262db318f19c524f45186dc8b041e59819730
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-04-03 23:20:38 -0300

    feat: add header to clients list

--- src/common/components/pages/clientes/clientes-area/clientes-area.tsx
+++ src/common/components/pages/clientes/clientes-area/clientes-area.tsx
@@ -1,48 +1,66 @@
 import { Footer } from '@components/application/dashboard/footer'
 import { PageTitle } from '@components/application/page-title'
 import { Status } from '@components/application/status'
+import { FiChevronDown } from 'react-icons/fi'
 import clients from 'src/common/utils/data/clients.json'
-import { Avatar, Container, ContentWrapper } from './styles'
+import { Avatar, Container, Content } from './styles'
 
 export const ClientesArea = () => {
     return (
         <Container>
             <PageTitle title="Clientes Cadastrados" />
 
-            <ContentWrapper>
-                <table>
-                    <tr>
-                        <th>Cadastrador por</th>
-                        <th>Cliente</th>
-                        <th>Cidade</th>
-                        <th>Data</th>
-                        <th>Status</th>
-                        <th>Ações</th>
-                    </tr>
+            <Content>
+                <header>
+                    <input placeholder="Procurar" />
 
-                    {clients.map((client) => (
-                        <tr key={client.id}>
-                            <td>
-                                <div>
-                                    <Avatar
-                                        src={client.sellerPicture}
-                                        width="35"
-                                        height="35"
-                                    />
-                                    <span>{client.seller}</span>
-                                </div>
-                            </td>
-                            <td>{client.name}</td>
-                            <td>{client.city}</td>
-                            <td>{client.createdAt}</td>
-                            <td>
-                                <Status orderStatus={client.status} />
-                            </td>
-                            <td>...</td>
+                    <div>
+                        <button>
+                            <span>Todas as datas</span>
+                            <FiChevronDown />
+                        </button>
+
+                        <button>
+                            <span>Status</span> <FiChevronDown />
+                        </button>
+                    </div>
+                </header>
+
+                <body>
+                    <table>
+                        <tr>
+                            <th>Cadastrador por</th>
+                            <th>Cliente</th>
+                            <th>Cidade</th>
+                            <th>Data</th>
+                            <th>Status</th>
+                            <th>Ações</th>
                         </tr>
-                    ))}
-                </table>
-            </ContentWrapper>
+
+                        {clients.map((client) => (
+                            <tr key={client.id}>
+                                <td>
+                                    <div>
+                                        <Avatar
+                                            src={client.sellerPicture}
+                                            width="35"
+                                            height="35"
+                                        />
+                                        <span>{client.seller}</span>
+                                    </div>
+                                </td>
+                                <td>{client.name}</td>
+                                <td>{client.city}</td>
+                                <td>{client.createdAt}</td>
+                                <td>
+                                    <Status orderStatus={client.status} />
+                                </td>
+                                <td>...</td>
+                            </tr>
+                        ))}
+                    </table>
+                </body>
+            </Content>
 
             <Footer />
         </Container>
--- src/common/components/pages/clientes/clientes-area/styles.ts
+++ src/common/components/pages/clientes/clientes-area/styles.ts
@@ -10,10 +10,68 @@ export const Container = styled.div`
     justify-content: space-between;
 `
 
-export const ContentWrapper = styled.div`
+export const Content = styled.div`
     background-color: ${({ theme }) => theme.colors.background_900};
     margin: 1.25rem;
     border-radius: 10px;
+
+    header {
+        width: 100%;
+        padding: 1.25rem;
+        background-color: ${({ theme }) => theme.colors.background_700};
+
+        border-bottom: 1px solid ${({ theme }) => theme.colors.background_900};
+        border-radius: 10px 10px 0 0;
+
+        display: flex;
+        justify-content: space-between;
+
+        input {
+            padding: 0.5rem 1rem;
+            border-radius: 5px;
+            border: none;
+
+            background-color: ${({ theme }) => theme.colors.background_800};
+            font-size: ${({ theme }) => theme.fontSize.text_sm};
+            color: ${({ theme }) => theme.fontColor.title};
+            font-weight: ${({ theme }) => theme.fontWeight.medium};
+
+            &::placeholder {
+                font-size: ${({ theme }) => theme.fontSize.text_sm};
+                color: ${({ theme }) => theme.fontColor.title};
+                font-weight: ${({ theme }) => theme.fontWeight.medium};
+            }
+        }
+
+        div {
+            button {
+                padding: 0.5rem 1rem;
+                border-radius: 5px;
+                border: none;
+
+                width: 13rem;
+
+                background-color: ${({ theme }) => theme.colors.background_800};
+                font-size: ${({ theme }) => theme.fontSize.text_sm};
+                color: ${({ theme }) => theme.fontColor.title};
+                font-weight: ${({ theme }) => theme.fontWeight.medium};
+
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                cursor: pointer;
+
+                span {
+                    margin-right: 1rem;
+                }
+
+                &:first-child {
+                    margin-right: 1rem;
+                }
+            }
+        }
+    }
 
     table {
         width: 100%;
